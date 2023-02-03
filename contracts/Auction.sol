5a9f60036e77cbcca6f69b339426c04b7f29f2a0
thelonehegelian <thelonehegelian@gmail.com>
2023-02-03 08:28:05 +0000

    Update Auction with some comments

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -11,7 +11,8 @@ pragma solidity ^0.8.9;
 @todo use openzeppelin's ownable
 @todo create frontend if time permits
 @note who receives the bid amount? the auctioneer or the contract itself?
-
+@todo update the contract: see requirements
+@todo run slither
 */
 
 contract Auction {
@@ -22,12 +23,13 @@ contract Auction {
         uint auctionId;
         Item item;
         uint256 highestBid;
-        address highestBidder;
+        address payable highestBidder; // @note is payable necessary? yes it is because we need to transfer money to the previous highest bidder
         uint256 auctionEndTime;
     }
     struct Item {
         string itemName;
         uint256 itemPrice; // starting price
+        // address highestBidder; @todo uncomment this and update the code
     }
 
     mapping(uint => Auctions) public auctions;
@@ -61,7 +63,7 @@ contract Auction {
         _auctions.item = item;
         // @note could be a mapping?
         _auctions.highestBid = item.itemPrice; // starting price, this way we won't have to check if the bid is higher than the starting price
-        _auctions.highestBidder = address(0); // not necessary, solidity does this by default
+        // _auctions.highestBidder = address (0); // not necessary, solidity does this by default
         _auctions.auctionEndTime = block.timestamp + 1 days;
 
         emit AuctionCreated(auctionId, item);
@@ -69,10 +71,14 @@ contract Auction {
 
     // * this function adds balance to the contract
     function placeBid(uint auctionId) public payable {
-        // require that auctionEndTime is not yet reached
+        // if the auction time has ended then find the highest bidder and transfer the item to the highest bidder
+        if (auctions[auctionId].auctionEndTime > block.timestamp) {
+            findHighestBidder(auctionId);
+        }
+
         require(
             auctions[auctionId].auctionEndTime > block.timestamp,
-            "Auction has ended."
+            "Auction has ended." // @todo use custom error to save gas
         );
 
         require(
@@ -85,8 +91,18 @@ contract Auction {
         );
         require(msg.sender != owner, "Owner cannott bid on their own auction."); // @todo use custom error to save gas
 
+        // if the above conditions are met then ...
+        address prevHighestBidder = auctions[auctionId].highestBidder;
+        uint256 prevHighestBid = auctions[auctionId].highestBid;
+
         auctions[auctionId].highestBid = msg.value;
-        auctions[auctionId].highestBidder = msg.sender;
+        auctions[auctionId].highestBidder = payable(msg.sender);
+
+        transferToOldBidder(payable(prevHighestBidder), prevHighestBid);
+
+        // !this won't work because the bidder can bid on multiple auctions (if I am using the Factory)
+        // !also how can I return the money using the mapping?
+        // bidders[msg.sender] = msg.value;
 
         emit BidPlaced(auctionId, msg.value, msg.sender);
     }
@@ -101,11 +117,17 @@ contract Auction {
      *  HELPERS *
      ************/
 
-    function findHighestBidder() private {
-        // is this function a requirement?
-        // has the auction ended?
-        // is the highest bid higher than the item price?
-        // is the highest bid higher than the highest bid?
-        // return money to all the bidders except the highest bidder
+    function findHighestBidder(uint _auctionId) private {
+        address auctionWinner = auctions[_auctionId].highestBidder;
+        uint256 highestBid = auctions[_auctionId].highestBid;
+        // @todo find the highest bidder for each item
+        emit AuctionEnded(_auctionId, highestBid, auctionWinner);
+    }
+
+    function transferToOldBidder(
+        address payable _prevBidder,
+        uint prevHighestBid
+    ) private {
+        _prevBidder.transfer(prevHighestBid);
     }
 }
