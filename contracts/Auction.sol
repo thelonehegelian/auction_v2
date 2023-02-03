05a3ff75cc9e29b799088d3af3dbc754d5b41871
thelonehegelian <thelonehegelian@gmail.com>
2023-02-03 06:42:20 +0000

    Add details to functions

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -17,29 +17,79 @@ contract Auction {
     // state variables
     address public owner;
 
-    struct AuctionDetails {
+    struct Auctions {
         uint auctionId;
         Item item;
+        uint256 highestBid;
+        address highestBidder;
+        uint256 auctionEndTime;
     }
     struct Item {
-        uint itemId;
         string itemName;
-        uint256 itemPrice;
+        uint256 itemPrice; // starting price
     }
+
+    mapping(uint => Auctions) public auctions;
+    mapping(address => uint) public bidders;
 
     constructor() {
         owner = msg.sender;
     }
 
-    function createAuction(string[] memory _auctionDetails) public onlyOwner {}
+    /***********
+     *  EVENTS *
+     ***********/
 
-    function placeBids() public {}
+    event AuctionCreated(uint auctionId, Item item);
+    event BidPlaced(uint auctionId, uint256 highestBid, address highestBidder);
+    event AuctionEnded(
+        uint auctionId,
+        uint256 highestBid,
+        address highestBidder
+    );
+
+    /*********
+     *  MAIN *
+     *********/
+
+    // !is this even gonna work?
+    function createAuction(uint auctionId, Item memory item) public onlyOwner {
+        Auctions storage _auctions = auctions[auctionId++];
+        _auctions.auctionId = auctionId;
+        _auctions.item = item;
+        _auctions.highestBid = 0; // not necessary, solidity does this by default
+        _auctions.highestBidder = address(0); // not necessary, solidity does this by default
+        _auctions.auctionEndTime = block.timestamp + 1 days;
+
+        emit AuctionCreated(auctionId, item);
+    }
+
+    function placeBids(uint auctionId) public payable {
+        require(
+            msg.value > auctions[auctionId].highestBid,
+            "Your bid is lower than the highest bid." // @todo use custom error to save gas
+        );
+
+        auctions[auctionId].highestBid = msg.value;
+        auctions[auctionId].highestBidder = msg.sender;
+
+        emit BidPlaced(auctionId, msg.value, msg.sender);
+    }
 
     // onlyOwner modifier
     modifier onlyOwner() {
-        require(msg.sender == owner, "Only owner can call this function.");
+        require(msg.sender == owner, "Only owner can call this function."); // @todo use custom error to save gas
         _;
     }
 
-    function findHighestBidder() private {}
+    /************
+     *  HELPERS *
+     ************/
+
+    function findHighestBidder() private {
+        // is this function a requirement?
+        // has the auction ended?
+        // is the highest bid higher than the item price?
+        // is the highest bid higher than the highest bid?
+    }
 }
