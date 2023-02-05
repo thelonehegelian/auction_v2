be67ee761525fa192c71fa48eeaaf36f8cefb8ce
thelonehegelian <thelonehegelian@gmail.com>
2023-02-05 08:14:13 +0000

    Update higher bid test

--- test/Auction.test.ts
+++ test/Auction.test.ts
@@ -34,6 +34,7 @@ const items = [
 
 const BID_AMOUNT_ONE = ethers.utils.parseEther('125');
 const BID_AMOUNT_TWO = ethers.utils.parseEther('150');
+const ACCOUNT_BALANCE = ethers.utils.parseEther('10000'); // 10,000 ETH for testing in hardhat
 
 describe('Auction', function () {
   // @note also possible to use hardhat-deploy
@@ -47,18 +48,22 @@ describe('Auction', function () {
   }
 
   describe('Deployment', function () {
-    it('Should creator as owner of contract', async function () {
+    beforeEach(async function () {
+      console.log('=========================================================');
+    });
+
+    it('should set deployer as owner of contract', async function () {
       const { auction, owner } = await loadFixture(deployAuctionFixture);
       expect(await auction.owner()).to.equal(owner.address);
     });
 
-    it('Should only allow owner of the contract to create auctions', async function () {
+    it('should only allow owner of the contract to create auctions', async function () {
       const { auction, bidder1 } = await loadFixture(deployAuctionFixture);
       await expect(auction.connect(bidder1).createAuction(items, auctionName))
         .to.be.reverted;
     });
 
-    it('Should create an auction', async function () {
+    it('should create an auction', async function () {
       const { auction } = await loadFixture(deployAuctionFixture);
 
       await auction.createAuction(items, auctionName);
@@ -68,7 +73,7 @@ describe('Auction', function () {
       expect(itemName).to.equal(items[0].itemName);
     });
 
-    it('Should not allow owner/bid creator to bid', async function () {
+    it('should not allow auction creator to bid', async function () {
       const { auction, owner } = await loadFixture(deployAuctionFixture);
       await auction.createAuction(items, auctionName);
       await expect(
@@ -76,19 +81,26 @@ describe('Auction', function () {
       ).to.be.reverted;
     });
 
-    it('Should not allow bid less than or equal to highest bid or the starting price', async function () {
+    it('should not allow bids less than or equal to highest bid or the starting price', async function () {
       const { auction, bidder1 } = await loadFixture(deployAuctionFixture);
       await auction.createAuction(items, auctionName);
       await expect(auction.connect(bidder1).placeBid(0, 1, { value: 90 })).to.be
         .reverted;
     });
 
-    it('Should update higher bid and bidder', async function () {
+    it('should update a higher bid and the bidder', async function () {
       const { auction, bidder1, bidder2 } = await loadFixture(
         deployAuctionFixture
       );
       await auction.createAuction(items, auctionName);
       await auction.connect(bidder1).placeBid(0, 1, { value: BID_AMOUNT_ONE });
+      const gasPrice = await ethers.provider.getGasPrice();
+      const gasFee = gasPrice.mul(BigNumber.from(23000));
+
+      // bidder1 balance should have reduced
+      let bidder1BalanceBefore = await bidder1.getBalance();
+      expect(bidder1BalanceBefore).to.lte(ACCOUNT_BALANCE.sub(BID_AMOUNT_ONE));
+
       let activeAuction = await auction.getAuction(0);
       let highestBidItem2 = activeAuction.items[1].highestBid;
       expect(highestBidItem2).to.equal(BID_AMOUNT_ONE);
@@ -96,9 +108,13 @@ describe('Auction', function () {
       activeAuction = await auction.getAuction(0);
       highestBidItem2 = activeAuction.items[1].highestBid;
       expect(highestBidItem2).to.equal(BID_AMOUNT_TWO);
+
+      // bidder1 should have been refunded, i am not taking precisse gas fee into account and dust into account here
+      const bidder1Balance = await bidder1.getBalance();
+      expect(bidder1Balance).to.gte(bidder1BalanceBefore);
     });
 
-    it('Should not allow bid after auction end time', async function () {
+    it('should not allow bids after auction end time', async function () {
       const { auction, bidder1 } = await loadFixture(deployAuctionFixture);
       const now = await time.latest();
       const twoDays = 172800;
